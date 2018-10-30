94b94fdd10055ef39b6f73aa9155b68fd981958f
f1r3starter <andrey.filenochko@gmail.com>
2018-10-30 21:12:57 +0200

    added spinner and api

--- source/REST/api.js
+++ source/REST/api.js
@@ -1,2 +1,90 @@
+import { MAIN_URL, TOKEN } from "./config";
+
 export const api = {
+    async fetchTasks () {
+        const response = await fetch(MAIN_URL, {
+            method:  'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization:  TOKEN,
+            },
+        });
+
+        if (response.status !== 200) {
+            throw new Error('Tasks were not not loaded.');
+        }
+
+        const { data: tasks } = await response.json();
+
+        return tasks;
+    },
+
+    async createTask (message) {
+        const response = await fetch(MAIN_URL, {
+            method:  'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization:  TOKEN,
+                data:           JSON.stringify({
+                    message,
+                }),
+            },
+        });
+
+        if (response.status !== 200) {
+            throw new Error('Task was not created');
+        }
+
+        const { data: task } = await response.json();
+
+        return task;
+    },
+
+    async updateTask (updatedTask) {
+        const response = await fetch(MAIN_URL, {
+            method:  'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization:  TOKEN,
+                data:           JSON.stringify([
+                    updatedTask
+                ]),
+            },
+        });
+
+        if (response.status !== 200) {
+            throw new Error('Task was not updated');
+        }
+
+        const { data: task } = await response.json();
+
+        return task;
+    },
+
+    async removeTask (id) {
+        const response = await fetch(`${MAIN_URL}/${id}`, {
+            method:  'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization:  TOKEN,
+            },
+        });
+
+        if (response.status !== 200 && response.status !== 204) {
+            throw new Error('Task was not removed');
+        }
+    },
+
+    async completeAllTasks (completedTasks) {
+        const fetchTasks = completedTasks.map((task) => fetch(MAIN_URL, {
+            method:  'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization:  TOKEN,
+                data:           JSON.stringify([task]),
+            },
+        }));
+
+        await Promise.all(fetchTasks);
+    },
 };
