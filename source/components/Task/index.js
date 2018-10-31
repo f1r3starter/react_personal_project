5cb71110dddf4df8cf23bee1c2c2328dfac12b2e
f1r3starter <andrey.filenochko@gmail.com>
2018-10-31 20:42:59 +0200

    added methods

--- source/components/Task/index.js
+++ source/components/Task/index.js
@@ -1,10 +1,22 @@
 // Core
 import React, { PureComponent } from 'react';
+import { string, func, bool } from 'prop-types';
 
 // Instruments
 import Styles from './styles.m.css';
 
 export default class Task extends PureComponent {
+    static propTypes = {
+        _removeTaskAsync: func.isRequired,
+        _updateTaskAsync: func.isRequired,
+        completed:        bool.isRequired,
+        created:          string.isRequired,
+        favorite:         bool.isRequired,
+        id:               string.isRequired,
+        message:          string.isRequired,
+        modified:         string.isRequired,
+    };
+
     _getTaskShape = ({
         id = this.props.id,
         completed = this.props.completed,
@@ -16,6 +28,42 @@ export default class Task extends PureComponent {
         favorite,
         message,
     });
+
+    _setTaskEditingState = () => {
+
+    };
+
+    _updateTask = () => {
+
+    };
+
+    _updateNewTaskMessage = () => {
+
+    };
+
+    _updateTaskMessageOnClick = () => {
+
+    };
+
+    _cancelUpdatingTaskMessage = () => {
+
+    };
+
+    _updateTaskMessageOnKeyDown = () => {
+
+    };
+
+    _toggleTaskCompletedState = () => {
+
+    };
+
+    _toggleTaskFavoriteState = () => {
+
+    };
+
+    _removeTask = () => {
+
+    };
 
     render () {
         return <li className = { Styles.task }>Задача: стартовая точка</li>;
